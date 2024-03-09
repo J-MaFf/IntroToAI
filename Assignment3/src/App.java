a302b85848004d0eafde25bf76a9df3be7e90e8b
Joey Maffiola <7maffiolajoey@gmail.com>
2024-03-09 11:05:51 -0600

    Adding skeleton methods.

--- Assignment3/src/App.java
+++ Assignment3/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        GeneticAlgorithm ga = new GeneticAlgorithm(); // Create an instance of the GeneticAlgorithm class
+        ga.run(); // Start the genetic algorithm process
     }
 }
--- Assignment3/src/GeneticAlgorithm.java
+++ Assignment3/src/GeneticAlgorithm.java
@@ -1,11 +1,27 @@
 public class GeneticAlgorithm {
     private Population population;
     private int populationSize = 4;
+    private int maxGenerations = 10; // May need to change
 
     public GeneticAlgorithm() {
         this.population = new Population(populationSize);
         this.population.initializePopulation();
     }
 
+    public void run() {
+        int generation = 0;
+        while (generation < maxGenerations) {
+            // Perform selection
+            // Perform crossover
+            // Optionally, perform mutation
+            // Evaluate fitness of new population
+            // Possibly update best solution found
+            // Prepare for next generation
+            generation++;
+        }
+        // Output the best solution found
+    }
+
     // Methods for running the GA, selection, crossover, etc., will go here
+
 }
