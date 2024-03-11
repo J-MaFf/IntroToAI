64d6cb09ab06a788d6fc58615943e62da2538394
Joey Maffiola <7maffiolajoey@gmail.com>
2024-03-11 14:20:10 -0500

    Added documentation to Main.java

---
+++ Assignment3/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    /**
+     * The main method of the program.
+     * It creates an instance of the GeneticAlgorithm class and starts the genetic
+     * algorithm process.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GeneticAlgorithm ga = new GeneticAlgorithm();
+        ga.run(); // Start the genetic algorithm process
+    }
+}
