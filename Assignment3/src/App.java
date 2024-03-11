1adbdad6a6dd359185c7729bf272d84fd76298c9
Joey Maffiola <7maffiolajoey@gmail.com>
2024-03-11 14:01:35 -0500

    Main.java edit

--- Assignment3/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        GeneticAlgorithm ga = new GeneticAlgorithm(); // Create an instance of the GeneticAlgorithm class
-        ga.run(); // Start the genetic algorithm process
-    }
-}
