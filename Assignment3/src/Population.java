5544bbcab85e42f6acad85caa6938b095b4ebb9b
Joey Maffiola <7maffiolajoey@gmail.com>
2024-03-11 14:15:40 -0500

    Added documentation to Population.java

--- Assignment3/src/Population.java
+++ Assignment3/src/Population.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a population of chromosomes.
+ */
 public class Population {
     private List<Chromosome> chromosomes;
 
+    /**
+     * Constructs a population with the specified size.
+     *
+     * @param size the size of the population
+     */
     public Population(int size) {
         chromosomes = new ArrayList<>(size);
         // Initialize with nulls to set the correct size
@@ -12,6 +20,11 @@ public class Population {
         }
     }
 
+    /**
+     * Initializes the population with random chromosomes of the specified length.
+     *
+     * @param chromosomeLength the length of each chromosome
+     */
     public void initializePopulation(int chromosomeLength) {
         for (int i = 0; i < chromosomes.size(); i++) {
             String binaryString = Integer.toBinaryString((int) (Math.random() * (Math.pow(2, chromosomeLength) - 1)));
@@ -24,10 +37,20 @@ public class Population {
         }
     }
 
+    /**
+     * Returns the list of chromosomes in the population.
+     *
+     * @return the list of chromosomes
+     */
     public List<Chromosome> getChromosomes() {
         return chromosomes;
     }
 
+    /**
+     * Sets the list of chromosomes in the population.
+     *
+     * @param newGeneration the new list of chromosomes
+     */
     public void setChromosomes(List<Chromosome> newGeneration) {
         this.chromosomes = newGeneration;
     }
