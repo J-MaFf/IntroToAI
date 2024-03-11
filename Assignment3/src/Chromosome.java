fe8ade947a831648edc664a7272c025531d216ed
Joey Maffiola <7maffiolajoey@gmail.com>
2024-03-11 13:58:56 -0500

    Chromosome.java edit

--- Assignment3/src/Chromosome.java
+++ Assignment3/src/Chromosome.java
@@ -4,7 +4,7 @@ public class Chromosome {
 
     public Chromosome(String binaryString) {
         this.binaryString = binaryString;
-        this.fitness = calculateFitness();
+        this.fitness = 0.0;
     }
 
     public String getBinaryString() {
@@ -19,10 +19,11 @@ public class Chromosome {
         return fitness;
     }
 
-    public double calculateFitness() {
-        // Convert binary string to integer value of x
-        int x = Integer.parseInt(binaryString, 2) - 10; // Adjust range from 0-20 to -10-10
-        // Calculate fitness (fitness is the value of the function f(x))
-        return Math.pow(x, 2) + 6 * x + 5;
+    public void calculateFitness() {
+        // Convert binary string to an integer value of x, considering the range [-10,
+        // 10]
+        int x = Integer.parseInt(binaryString, 2) - 10;
+        // Calculate the function value as fitness
+        this.fitness = Math.pow(x, 2) + 6 * x + 5;
     }
 }
