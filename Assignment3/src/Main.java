7f5e024f7089c31d07b1d67fa77454efa540992a
Joey M <7maffiolajoey@gmail.com>
2024-05-04 23:14:27 -0500

    Revert "tryThis"

--- Assignment3/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        GeneticAlgorithm ga = new GeneticAlgorithm();
-        ga.run(); // Start the genetic algorithm process
-    }
-}
