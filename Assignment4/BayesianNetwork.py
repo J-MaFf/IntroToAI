ac583926cfb06669d33fb16a6e89a972c080ff09
Joey Maffiola <7maffiolajoey@gmail.com>
2024-05-05 19:59:25 -0500

    feat: Refactor create_cpt_node method in BayesianNetwork class
    This commit refactors the create_cpt_node method in the BayesianNetwork class. The method is responsible for creating a CPT (Conditional Probability Table) node in the network. The method signature has been changed to follow the Python naming convention, and the code has been reorganized for better readability and maintainability.
    
    This refactoring improves the overall structure of the code and ensures consistency with the rest of the class.

--- Assignment4/BayesianNetwork.py
+++ Assignment4/BayesianNetwork.py
@@ -1,4 +1,4 @@
-import pysmile as ps
+import pysmile
 
 
 class BayesianNetwork:
@@ -17,12 +17,12 @@ class BayesianNetwork:
         Returns:
             None
         """
-        self.net = ps.Network()
+        self.net = pysmile.Network()
         self.name = name
         self.createNodes()
         self.createArcs()
 
-    def create_cpt_node(self, net, id, name, outcomes, x_pos, y_pos):
+    def createCptNode(self, net, id, name, outcomes, x_pos, y_pos):
         """
         Create a CPT (Conditional Probability Table) node in the network.
 
@@ -39,7 +39,7 @@ class BayesianNetwork:
 
         """
         handle = net.add_node(  # It is called handle because it is a reference to the node in the network
-            ps.NodeType.CPT,
+            pysmile.NodeType.CPT,
             id,  # This creates a node with a Conditional Probability Table (CPT)
         )
 
@@ -69,37 +69,35 @@ class BayesianNetwork:
             None
         """
         # Create Geographical_area node
-        self.geographialAreaNode = self.create_cpt_node(
+        self.geographialAreaNode = self.createCptNode(
             self.net, 0, "Geographical Area", ["Centre", "North", "South islands"], 50, 50
         )
 
         # Create Education node
-        self.educationNode = self.create_cpt_node(
-            self.net, 1, "Education", ["High", "Low"], 100, 50
-        )
+        self.educationNode = self.createCptNode(self.net, 1, "Education", ["High", "Low"], 100, 50)
 
         # Create Allergy node
-        self.allergyNode = self.create_cpt_node(self.net, 2, "Allergy", ["No", "Yes"], 150, 50)
+        self.allergyNode = self.createCptNode(self.net, 2, "Allergy", ["No", "Yes"], 150, 50)
 
         # Create Smoke node
-        self.smokeNode = self.create_cpt_node(self.net, 3, "Smoke", ["No", "Yes"], 200, 50)
+        self.smokeNode = self.createCptNode(self.net, 3, "Smoke", ["No", "Yes"], 200, 50)
 
         # Create Sedentary node
-        self.sedentaryNode = self.create_cpt_node(self.net, 4, "Sedentary", ["No", "Yes"], 250, 50)
+        self.sedentaryNode = self.createCptNode(self.net, 4, "Sedentary", ["No", "Yes"], 250, 50)
 
         # Create Age node
-        self.ageNode = self.create_cpt_node(self.net, 5, "Age", ["Adult", "Old", "Young"], 300, 50)
+        self.ageNode = self.createCptNode(self.net, 5, "Age", ["Adult", "Old", "Young"], 300, 50)
 
         # Create Sex node
-        self.sexNode = self.create_cpt_node(self.net, 6, "Sex", ["Female", "Male"], 350, 50)
+        self.sexNode = self.createCptNode(self.net, 6, "Sex", ["Female", "Male"], 350, 50)
 
         # Create Urbanization node
-        self.urbanizationNode = self.create_cpt_node(
+        self.urbanizationNode = self.createCptNode(
             self.net, 7, "Urbanization", ["High", "Low", "Medium"], 400, 50
         )
 
         # Create Prior for Asthma node
-        self.asthmaNode = self.create_cpt_node(self.net, 8, "Asthma", ["No", "Yes"], 450, 50)
+        self.asthmaNode = self.createCptNode(self.net, 8, "Asthma", ["No", "Yes"], 450, 50)
 
     def createArcs(self):
         """
@@ -119,3 +117,76 @@ class BayesianNetwork:
         self.net.add_arc(self.asthmaNode, self.ageNode)  # Asthma -> Age
         self.net.add_arc(self.asthmaNode, self.sexNode)  # Asthma -> Sex
         self.net.add_arc(self.asthmaNode, self.urbanizationNode)  # Asthma -> Urbanization
+
+    def setNodeDefinition(self):
+        geographicalAreaDef = [
+            0.18690401,  # P(Geographical Area=Centre|Asthma=No)
+            0.44310235,  # P(Geographical Area=North|Asthma=No)
+            0.36999364,  # P(Geographical Area=South islands|Asthma=No)
+            0.20558376,  # P(Geographical Area=Centre|Asthma=Yes)
+            0.39509306,  # P(Geographical Area=North|Asthma=Yes)
+            0.39932318,  # P(Geographical Area=South islands|Asthma=Yes)
+        ]
+        self.net.set_node_definition(self.geographialAreaNode, geographicalAreaDef)
+
+        educationDef = [
+            0.50095359,  # P(Education=High|Asthma=No)
+            0.49904641,  # P(Education=Low|Asthma=No)
+            0.40270728,  # P(Education=High|Asthma=Yes)
+            0.59729272,  # P(Education=Low|Asthma=Yes)
+        ]
+        self.net.set_node_definition(self.educationNode, educationDef)
+
+        allergyDef = [
+            0.87857597,  # P(Allergy=No|Asthma=No)
+            0.12142403,  # P(Allergy=Yes|Asthma=No)
+            0.5177665,  # P(Allergy=No|Asthma=Yes)
+            0.4822335,  # P(Allergy=Yes|Asthma=Yes)
+        ]
+        self.net.set_node_definition(self.allergyNode, allergyDef)
+
+        smokeDef = [
+            0.84678957,  # P(Smoke=No|Asthma=No)
+            0.15321043,  # P(Smoke=Yes|Asthma=No)
+            0.83671743,  # P(Smoke=No|Asthma=Yes)
+            0.16328257,  # P(Smoke=Yes|Asthma=Yes)
+        ]
+        self.net.set_node_definition(self.smokeNode, smokeDef)
+
+        sedentaryDef = [
+            0.24920534,  # P(Sedentary=No|Asthma=No)
+            0.75079466,  # P(Sedentary=Yes|Asthma=No)
+            0.21150592,  # P(Sedentary=No|Asthma=Yes)
+            0.78849408,  # P(Sedentary=Yes|Asthma=Yes)
+        ]
+        self.net.set_node_definition(self.sedentaryNode, sedentaryDef)
+
+        ageDef = [
+            0.54609027,  # P(Age=Adult|Asthma=No)
+            0.33121424,  # P(Age=Old|Asthma=No)
+            0.12269549,  # P(Age=Young|Asthma=No)
+            0.45939086,  # P(Age=Adult|Asthma=Yes)
+            0.43824027,  # P(Age=Old|Asthma=Yes)
+            0.10236887,  # P(Age=Young|Asthma=Yes)
+        ]
+        self.net.set_node_definition(self.ageNode, ageDef)
+
+        sexDef = [
+            0.52765416,
+            0.47234584,
+            0.54060914,
+            0.45939086,
+        ]
+        self.net.set_node_definition(self.sexNode, sexDef)
+
+        urbanizationDef = [
+            0.30769231,  # P(Urbanization=High|Asthma=No)
+            0.27908455,  # P(Urbanization=Low|Asthma=No)
+            0.41322314,  # P(Urbanization=Medium|Asthma=No)
+            0.3248731,  # P(Urbanization=High|Asthma=Yes)
+            0.27580372,  # P(Urbanization=Low|Asthma=Yes)
+            0.39932318,  # P(Urbanization=Medium|Asthma=Yes)
+        ]
+        self.net.set_node_definition(self.urbanizationNode, urbanizationDef)
+
+        asthmaDef = [0.57096189, 0.42903811]  # P(Asthma=No), P(Asthma=Yes)
